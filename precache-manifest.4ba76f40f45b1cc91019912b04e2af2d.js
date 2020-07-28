e5ce427c514377805b15547ffb02c5e6a1267dea
Zach Waite <zach@waiteperspectives.com>
2020-07-28 10:20:54 -0400

    Updates

---
+++ precache-manifest.4ba76f40f45b1cc91019912b04e2af2d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde2aa5c126ee0dc74f33bc3799d93b2",
+    "url": "/lakespeed/index.html"
+  },
+  {
+    "revision": "b92962635605f56000d5",
+    "url": "/lakespeed/static/css/2.99d4d416.chunk.css"
+  },
+  {
+    "revision": "8edcad9b456ce8fb7dd8",
+    "url": "/lakespeed/static/css/main.d15a9799.chunk.css"
+  },
+  {
+    "revision": "b92962635605f56000d5",
+    "url": "/lakespeed/static/js/2.be6a02b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lakespeed/static/js/2.be6a02b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8edcad9b456ce8fb7dd8",
+    "url": "/lakespeed/static/js/main.6846fcf4.chunk.js"
+  },
+  {
+    "revision": "46dbac41623bfe796856",
+    "url": "/lakespeed/static/js/runtime-main.a616964e.js"
+  }
+]);
