72faef8b47f74aa177753d114217e2008480d906
Zach Waite <zach@waiteperspectives.com>
2020-07-28 10:38:39 -0400

    imp: add basic description

--- src/App.js
+++ src/App.js
@@ -8,12 +8,18 @@ const App = () => {
   return (
     <div className="App">
       <section className="App-header-section">
-        <h1>Historical Data</h1>
+        <p><strong>Historical Speedtest Results:</strong></p>
+	<p>2219 County Route 47,  Salem, NY</p>
       </section>
       <section className="App-grid-section">
         <div className="App-grid-container">
           <ResultsGrid />
         </div>
+      </section>
+      <section className="App-footer-section">
+	<div className="App-footer-container">
+          <p>Data acquired using <a href="https://www.speedtest.net/apps/cli">Speedtest</a></p>
+	</div>
       </section>
     </div>
   );
