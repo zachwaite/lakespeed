8483281faba56aa0caa7777fc6dc1b81f1d05446
Zach Waite <zach@waiteperspectives.com>
2020-09-23 21:06:22 -0400

    Updates

--- precache-manifest.25285e039b24faaf93554aa2515e3956.js
+++ precache-manifest.25285e039b24faaf93554aa2515e3956.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c40c9657683a53a017d0956de909765c",
+    "revision": "0d948a7396973090917f91b1f91c8596",
     "url": "/lakespeed/index.html"
   },
   {
-    "revision": "d097871e6e6cd6e2c7c6",
+    "revision": "02d904299e8795af05a1",
     "url": "/lakespeed/static/css/main.f404bca2.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/lakespeed/static/js/2.a8a2a67b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d097871e6e6cd6e2c7c6",
-    "url": "/lakespeed/static/js/main.4d093da3.chunk.js"
+    "revision": "02d904299e8795af05a1",
+    "url": "/lakespeed/static/js/main.23aa019c.chunk.js"
   },
   {
     "revision": "46dbac41623bfe796856",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lakespeed/precache-manifest.083021a3ae7fef976c2b9d1bca2fdb56.js"
+  "/lakespeed/precache-manifest.25285e039b24faaf93554aa2515e3956.js"
 );
 
 self.addEventListener('message', (event) => {
